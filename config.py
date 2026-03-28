65ce1f39c1bd72698222ef3cb321a5d3b0d4c2e6
PersonaWoodoo <shortsik44@gmail.com>
2026-03-29 00:32:27 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+"""
+Конфигурация для Telegram бота
+"""
+
+# Telegram Bot Token
+BOT_TOKEN = "8474383852:AAGt7STDde1t1_OkYWE6oEyeOZumaqHMwkQ"
+
+# ID администраторов (можно добавить несколько через запятую)
+ADMIN_IDS = [8478884644, 5277086252]
